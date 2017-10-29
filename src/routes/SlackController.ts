6586aa7a2355c60ca805a2b4e175a98dc06102f7
Andy Wendt <andy@awendt.com>
2017-10-29 17:12:02 -0600

    Update SlackController

--- src/routes/SlackController.ts
+++ src/routes/SlackController.ts
@@ -1,11 +1,27 @@
-import {Router, Request, Response, NextFunction} from 'express';
+import {NextFunction, Response} from 'express';
+import {Request} from '../App';
+import {EventHandler, EventHandlerInterface, EventInterface} from "../Slack/EventHandler";
+import {ChallengeManager} from "../Challenge/ChallengeManager";
+import {InvalidEventError} from "../Errors/InvalidEventError";
 
 export class SlackController {
-    public events(req: Request, res: Response, next: NextFunction) {
-        return res.status(200).json({
-            foo: "bar"
+    eventHandler: EventHandlerInterface;
+
+    constructor(eventHandler: EventHandlerInterface) {
+        this.eventHandler = eventHandler;
+    }
+
+    public events = (req: Request, res: Response, next: NextFunction) => {
+        this.eventHandler.handle(<EventInterface> req.body.event).then((result: boolean) => {
+            return res.status(200).json({handled: true});
+        }).catch((error: any) => {
+            if (error instanceof InvalidEventError) {
+                return res.status(200).json({handled: true});
+            }
+
+            return res.status(500).json(error);
         });
     }
 }
 
-export default new SlackController();
+export default new SlackController(new EventHandler(new ChallengeManager()));
