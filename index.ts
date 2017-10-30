affe27209cce651a9503c6966fe4c3571bf4aee3
Andy Wendt <andy@awendt.com>
2017-10-30 00:33:45 -0600

    Fix regeneratorRuntime error

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+// was getting a "ReferenceError: regeneratorRuntime is not defined" error
+// requiring 'babel-polyfill' was the quick fix
+require('babel-polyfill');
+
 import * as Webtask from "webtask-tools";
 import App from "./src/App";
 
