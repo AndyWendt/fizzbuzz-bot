77a7e153b3c112f03b865741a59062e9553efb73
Andy Wendt <andy@awendt.com>
2017-10-29 21:21:54 -0600

    Add Middleware.decodePayload()

--- src/Middleware.ts
+++ src/Middleware.ts
@@ -1,7 +1,15 @@
-import {Router, Response, NextFunction} from 'express';
+import {NextFunction, Response} from 'express';
 import {Request} from './App';
 
 export default class Middleware {
+    public static decodePayload(req: Request, res: Response, next: NextFunction) {
+        if (typeof req.body.payload === 'string') {
+            req.body = JSON.parse(req.body.payload);
+        }
+
+        next();
+    }
+
     public static checkBodyToken(req: Request, res: Response, next: NextFunction) {
         if (req.body.token !== req.webtaskContext.secrets.SLACK_VERIFICATION_TOKEN) {
             return res.status(401).json({
