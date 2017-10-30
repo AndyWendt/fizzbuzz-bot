238164a990ba1052df031c6830d444133252d12e
Andy Wendt <andy@awendt.com>
2017-10-29 18:27:02 -0600

    Add fizzbuzz challenge

--- src/Challenge/FizzBuzzChallenge.ts
+++ src/Challenge/FizzBuzzChallenge.ts
@@ -1,16 +1,33 @@
 import {ChallengeInterface} from "./ChallengeManager";
 
 export class FizzBuzzChallenge implements ChallengeInterface {
+
+    lowerLimit: number;
+    upperLimit: number;
+
+    constructor() {
+        this.lowerLimit = this.randomInt(1, 10000);
+        this.upperLimit = this.lowerLimit + 100;
+    }
+
     public rules(): string {
         return 'Rules';
     }
 
     public prepCode(code: string): string {
-        return code;
+        return code + `\r\n fizzbuzz(${this.lowerLimit}, ${this.upperLimit});`;
     }
 
     public passes(result: Array<any>): boolean {
+        // if (result.length !== 100) {
+        //     return false;
+        // }
+
         return true;
+    }
+
+    private randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
 }
