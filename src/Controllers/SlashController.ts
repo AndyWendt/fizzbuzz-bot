6bf18de6245c562f036c437e9f6b1f21bc603486
Andy Wendt <andy@awendt.com>
2017-10-29 21:41:48 -0600

    Update slash controller with options

--- src/Controllers/SlashController.ts
+++ src/Controllers/SlashController.ts
@@ -7,7 +7,28 @@ export class SlashController {
     }
 
     public slash = (req: Request, res: Response, next: NextFunction) => {
-        return res.status(200).json({handled: true});
+        return res.status(200).json({
+            text: '*Which challenge would you like to try?*',
+            attachments: [
+                {
+                    color: '#5A352D',
+                    callback_id: 'options',
+                    text: '', // attachments must have text property defined (abstractable)
+                    actions: [
+                        {
+                            name: 'select_type',
+                            type: 'select',
+                            options: [
+                                {
+                                    "text": "FizzBuzz",
+                                    "value": "fizzbuzz"
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        });
     }
 }
 
